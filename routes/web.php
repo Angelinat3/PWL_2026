46e9a6b5fa2472919d7f5818aa63fed955e598ad
Angelina Thennessa <fangelinatj@gmail.com>
2026-02-24 19:55:40 +0700

    Route Parameters

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+   return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'world';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'My name'.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Post to'.$postId." Comments to: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Article Page with ID '.$id;
 });
