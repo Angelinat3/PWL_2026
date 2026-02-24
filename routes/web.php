6b0e7bf3006137cfd2aea6e7ef47bb773a18f954
Angelina Thennessa <fangelinatj@gmail.com>
2026-02-24 22:45:51 +0700

    Web update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
 use Symfony\Component\Routing\Router;
 
 Route::get('/hello', [WelcomeController::class,'hello']);
@@ -29,3 +30,5 @@ return 'My name'.$name;
 Route::get('/about', [AboutController::class,'about']);
 Route::get('/', [HomeController::class,'index']);
 Route::get('/articles/{id}', [ArticleController::class,'articles']);
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
